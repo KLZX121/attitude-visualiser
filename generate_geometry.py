4d3bd0c81523df6c019eaaa5b97f3d2cbe43388f
KLZX121 <kenneth121lzx@gmail.com>
2026-09-12 22:13:54 +1000

    added normal vectors to surfaces when generating geometry

--- generate_geometry.py
+++ generate_geometry.py
@@ -43,8 +43,25 @@ def gen_cubesat(Lx=0.1, Ly=0.1, Lz=0.3405, gen_solar_panels=True, solar_panel_he
     y_neg
   ]
 
+  # define surface normal unit vectors
+  ux = [1, 0, 0]
+  uxn = [-1, 0, 0]
+  uy = [0, 1, 0]
+  uyn = [0, -1, 0]
+  uz = [0, 0, 1]
+  uzn = [0, 0, -1]
+  normals = [
+    uz,
+    uzn,
+    ux,
+    uxn,
+    uy,
+    uyn
+  ]
+
   # solar panel surface vertices
   # assumes two longitudinally deployed panels on the +x face
+  # defines two of each panel (for four panels) to accommodate double sided normals
   if gen_solar_panels:
     panel_y_pos = [vert[0], vert[4]]
     panel_y_pos += (np.array(panel_y_pos) + [0, solar_panel_height, 0]).tolist()[::-1]
@@ -52,19 +69,22 @@ def gen_cubesat(Lx=0.1, Ly=0.1, Lz=0.3405, gen_solar_panels=True, solar_panel_he
     panel_y_neg = [vert[1], vert[5]]
     panel_y_neg += (np.array(panel_y_neg) - [0, solar_panel_height, 0]).tolist()[::-1]
 
-    surfaces.extend([panel_y_pos, panel_y_neg])
+    surfaces.extend([panel_y_pos, panel_y_pos, panel_y_neg, panel_y_neg])
 
-  return surfaces
+    normals.extend([ux, uxn, ux, uxn])
+
+  return surfaces, normals
 
 
 # write data to json file
-def write_json(surfaces, names):
+def write_json(surfaces, normals, names):
   surface_objs = []
 
   for i, surf in enumerate(surfaces):
     surf_obj = {
       'name': names[i],
-      'vertices': surf
+      'vertices': surf,
+      'normal': normals[i]
     }
 
     surface_objs.append(surf_obj)
@@ -76,4 +96,20 @@ def write_json(surfaces, names):
   with open('geometry.json', 'w') as f:
     json.dump(data, f)
 
-write_json(gen_cubesat(), ['body_z_pos', 'body_z_neg', 'body_x_pos', 'body_x_neg', 'body_y_pos', 'body_y_neg', 'panel_y_pos', 'panel_y_neg'])
+surfaces, normals = gen_cubesat()
+write_json(
+  surfaces, 
+  normals, 
+  [
+    'body_z_pos', 
+    'body_z_neg', 
+    'body_x_pos', 
+    'body_x_neg', 
+    'body_y_pos', 
+    'body_y_neg', 
+    'panel_y_pos_x_pos', 
+    'panel_y_pos_x_neg', 
+    'panel_y_neg_x_pos', 
+    'panel_y_neg_x_neg'
+  ]
+)
